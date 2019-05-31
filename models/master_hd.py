ec9002fa63b3a3d7874570ef07c1cf1aea0ab5f4
Omar Gamaliel <g4w4@MacBook-Pro-de-Omar.local>
2019-05-30 19:36:14 -0500

    integration whit clientSocket

---
+++ models/master_hd.py
@@ -0,0 +1,18 @@
+import os, sys, time, json
+import config
+from Utils import logs
+
+class start():
+    socketIO = None
+    wsp = None
+    driver = None
+    awaitLogin = None
+
+    def __init__(self,socket): 
+        if not os.path.exists(config.pathSession): os.makedirs(config.pathSession)
+        self.socketIO = socket
+
+    def on_connect(*args):
+        print(args)
+        logs.logError('Socket-Info','Connection whit server')
+        self.socketIO.emit('Auth',"token")
--- services/client.py
+++ services/client.py
@@ -9,24 +9,14 @@ sys.path.insert(0,config.masterPath)
 
 # system modules #
 from Utils import logs
+from models import master_hd
 
 
-class start():
-    socketIO = None
-    wsp = None
-    driver = None
-    awaitLogin = None
+socketIO = SocketIO(config.ip,3000, LoggingNamespace)
+masterClass = master_hd.start(socketIO)
 
-    def __init__(self,socket,driver): 
-        if not os.path.exists(config.pathSession): os.makedirs(config.pathSession)
-        self.socketIO = SocketIO(config.ip,3000, LoggingNamespace)
-
-    def on_connect(*args):
-        logs.logError('Socket-Info','Connection whit server')
-        self.socketIO.emit('Auth',"token")
-
-    ##### SOCKET LISSENER #####
-    socketIO.on('connect', on_connect)
+##### SOCKET LISSENER #####
+socketIO.on('connect', masterClass.on_connect)
     # socketIO.on('welcome', on_welcome)
     # socketIO.on('reconnect', on_reconnect)
     # socketIO.on('getQr',on_getQr)
@@ -36,7 +26,4 @@ class start():
     # socketIO.on('sendFile',on_sendFile)
     # socketIO.on('deleteChat',on_deleteChat)
 
-    socketIO.wait()
-
-
-start()
+socketIO.wait()
