3ecf205877ed76585ed0f017e780e49d13697c50
Omar Gamaliel <g4w4@MacBook-Pro-de-Omar.local>
2019-05-30 20:23:06 -0500

    auth server socket

--- models/master_hd.py
+++ models/master_hd.py
@@ -13,19 +13,19 @@ class start():
     wsp = None
     driver = None
     awaitLogin = None
+    __Keyword = "Master-Info"
 
     def __init__(self,socket): 
         if not os.path.exists(config.pathSession): os.makedirs(config.pathSession)
         self.socketIO = socket
 
     def on_connect(self,*args):
-        print(args)
         logs.logError('Socket-Info','Connection whit server')
         self.socketIO.emit('Auth',config.token)
 
     def on_welcome(self,*args):
         try:
-            logs.logError('Socket-Info','Connection success')
+            logs.logError(self.__Keyword,'Connection success')
             if self.driver != None and self.driver.is_logged_in():
                 # Is reconnection send all data #
                 self.socketIO.emit('change',_wapi.getGeneralInfo(self.driver))
@@ -38,16 +38,23 @@ class start():
                 # It's the first connection, try to remember session #
                 self.socketIO.emit('change',{"whatsAppJoin":False,"accountDown":False})
                 self.driver = WhatsAPIDriver(profile=config.pathSession, client='remote', command_executor=config.selemiunIP)
-                logs.logError('Master-Info','Check if have cache')
+                logs.logError(self.__Keyword,'Check if have cache')
                 rember = _wapi.rememberSession(self.driver)
 
                 if rember :
                     self.socketIO.emit('change',_wapi.getGeneralInfo(self.driver))
                 else : 
-                    logs.logError('Master-Info','Session down')
+                    logs.logError(self.__Keyword,'Session down')
                     # ALERT #
 
 
         except Exception :
             logs.logError('Master-Error',traceback.format_exc())
             # ALERTA #
+
+    def on_disconnect(self,*args):
+        logs.logError(self.__Keyword,'Connection end')
+
+    def on_reconnect(self,*args):
+        logs.logError(self.__Keyword,'Connection reconnect')
+        self.socketIO.emit('Auth',config.token)
