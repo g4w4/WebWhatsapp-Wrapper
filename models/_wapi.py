56d77f1779da2ff96ac8e791f54cd9ae2f66cfce
Omar Gamaliel <g4w4@MacBook-Pro-de-Omar.local>
2019-06-11 19:30:54 -0500

    logs Bug save_media

--- models/_wapi.py
+++ models/_wapi.py
@@ -111,6 +111,8 @@ def getOldMessages(driver):
             chats[idChat] = []
             _messages = driver.get_all_messages_in_chat(idChat,True)
             for message in _messages:
+                print(message)
+                print(dir(message))
                 body = interface_messages.getFormat(message,driver)
                 chats[idChat].append(body)
 
