a72247c5e23ba672e2ececc70089c4b31e30c836
g4w4 <gamaz00001@gmail.com>
2019-06-27 19:33:03 -0500

    getOldMessages

--- interfaces/interface_messages.py
+++ interfaces/interface_messages.py
@@ -23,7 +23,8 @@ __DOCUMENT_TYPE = {
 def getFormat(message,driver):
     body = {}
     try:
-        print(dir(message))
+        print(message.get('id',False))
+        print(message.get('type','NO hat type'))
         body['chat'] = message._js_obj.get('chat').get('id').get('_serialized'),
         body['sendBy'] =  True if driver.get_phone_number() in message.sender.id else False
         body['message'] = str(message.save_media(config.pathFiles,True)) if message.type != "chat" else message.content
