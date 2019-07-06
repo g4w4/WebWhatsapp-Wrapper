252701b02442c89a42d17aceab78e7d275f76b35
g4w4 <gamaz00001@gmail.com>
2019-07-06 00:08:27 -0500

    issue sendBy document

--- interfaces/interface_messages.py
+++ interfaces/interface_messages.py
@@ -26,6 +26,8 @@ class IdMessage():
     def get(self):
         _id = self.message.id.split("_")
         whoSend = self.message.sender._js_obj['formattedName']
+        print(self.message.sender)
+        print(dir(self.message.sender))
         print("more",self.message)
         print("other",_id)
         print("whoSend",self._idMessage["sendBy"])
