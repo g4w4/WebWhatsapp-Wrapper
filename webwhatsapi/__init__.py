e085db828cd58a4db212c36bc8e186111651d956
g4w4 <gamaz00001@gmail.com>
2020-02-04 17:57:42 -0600

    fix

--- webwhatsapi/__init__.py
+++ webwhatsapi/__init__.py
@@ -625,7 +625,6 @@ class WhatsAPIDriver(object):
         imgBase64 = self.convert_to_base64(path)
         filename = os.path.split(path)[-1]
         print("AHANAJAJAJ")
-        print( imgBase64 )
         t = self.wapi_functions.sendImage(imgBase64, chatid, filename, caption)
         print("SEEEE ENVIADAAAAAA NO MAAAAAA")
         print( t )
